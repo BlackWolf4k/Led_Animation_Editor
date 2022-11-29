cd385998074cdc8a46fff3b04458b41b7a24eeb5
BlackWolf4k <AfzdCOLssI@protonmail.com>
2022-11-29 23:09:19 +0100

    Started the socket development

--- Led_Animation_Editor/Form1.Designer.cs
+++ Led_Animation_Editor/Form1.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.animations_p = new System.Windows.Forms.Panel();
+            this.update_b = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.slaves_lv = new System.Windows.Forms.ListView();
             this.animation_settings_p = new System.Windows.Forms.Panel();
             this.repetitions_tb = new System.Windows.Forms.TrackBar();
             this.repetitions_l = new System.Windows.Forms.Label();
@@ -39,17 +40,19 @@
             this.phases_nud = new System.Windows.Forms.NumericUpDown();
             this.delay_nud = new System.Windows.Forms.NumericUpDown();
             this.animation_maker_p = new System.Windows.Forms.Panel();
+            this.colors_lb = new System.Windows.Forms.ListBox();
             this.color_b = new System.Windows.Forms.Button();
             this.to_l = new System.Windows.Forms.Label();
             this.from_l = new System.Windows.Forms.Label();
             this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
             this.save_b = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.phases_lb = new System.Windows.Forms.ListBox();
             this.devices_p = new System.Windows.Forms.Panel();
-            this.listView2 = new System.Windows.Forms.ListView();
+            this.animations_lv = new System.Windows.Forms.ListView();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.update_animations_b = new System.Windows.Forms.Button();
             this.animations_p.SuspendLayout();
             this.animation_settings_p.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.repetitions_tb)).BeginInit();
@@ -63,27 +66,38 @@
             // 
             // animations_p
             // 
+            this.animations_p.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.animations_p.Controls.Add(this.update_b);
             this.animations_p.Controls.Add(this.textBox1);
-            this.animations_p.Controls.Add(this.listView1);
+            this.animations_p.Controls.Add(this.slaves_lv);
             this.animations_p.Location = new System.Drawing.Point(572, 12);
             this.animations_p.Name = "animations_p";
             this.animations_p.Size = new System.Drawing.Size(200, 215);
             this.animations_p.TabIndex = 0;
             // 
+            // update_b
+            // 
+            this.update_b.Location = new System.Drawing.Point(142, 0);
+            this.update_b.Name = "update_b";
+            this.update_b.Size = new System.Drawing.Size(58, 23);
+            this.update_b.TabIndex = 4;
+            this.update_b.Text = "Update";
+            this.update_b.UseVisualStyleBackColor = true;
+            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 23);
+            this.textBox1.Size = new System.Drawing.Size(136, 23);
             this.textBox1.TabIndex = 1;
             // 
-            // listView1
+            // slaves_lv
             // 
-            this.listView1.Location = new System.Drawing.Point(0, 29);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(197, 183);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.slaves_lv.Location = new System.Drawing.Point(0, 29);
+            this.slaves_lv.Name = "slaves_lv";
+            this.slaves_lv.Size = new System.Drawing.Size(197, 183);
+            this.slaves_lv.TabIndex = 0;
+            this.slaves_lv.UseCompatibleStateImageBehavior = false;
             // 
             // animation_settings_p
             // 
@@ -139,6 +153,7 @@
             this.phases_nud.Name = "phases_nud";
             this.phases_nud.Size = new System.Drawing.Size(70, 23);
             this.phases_nud.TabIndex = 2;
+            this.phases_nud.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // delay_nud
             // 
@@ -146,24 +161,35 @@
             this.delay_nud.Name = "delay_nud";
             this.delay_nud.Size = new System.Drawing.Size(70, 23);
             this.delay_nud.TabIndex = 0;
+            this.delay_nud.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // animation_maker_p
             // 
+            this.animation_maker_p.Controls.Add(this.colors_lb);
             this.animation_maker_p.Controls.Add(this.color_b);
             this.animation_maker_p.Controls.Add(this.to_l);
             this.animation_maker_p.Controls.Add(this.from_l);
             this.animation_maker_p.Controls.Add(this.numericUpDown5);
             this.animation_maker_p.Controls.Add(this.numericUpDown4);
             this.animation_maker_p.Controls.Add(this.save_b);
-            this.animation_maker_p.Controls.Add(this.listBox1);
+            this.animation_maker_p.Controls.Add(this.phases_lb);
             this.animation_maker_p.Location = new System.Drawing.Point(12, 66);
             this.animation_maker_p.Name = "animation_maker_p";
             this.animation_maker_p.Size = new System.Drawing.Size(554, 383);
             this.animation_maker_p.TabIndex = 2;
             // 
+            // colors_lb
+            // 
+            this.colors_lb.FormattingEnabled = true;
+            this.colors_lb.ItemHeight = 15;
+            this.colors_lb.Location = new System.Drawing.Point(0, 199);
+            this.colors_lb.Name = "colors_lb";
+            this.colors_lb.Size = new System.Drawing.Size(395, 184);
+            this.colors_lb.TabIndex = 13;
+            // 
             // color_b
             // 
-            this.color_b.Location = new System.Drawing.Point(382, 167);
+            this.color_b.Location = new System.Drawing.Point(441, 103);
             this.color_b.Name = "color_b";
             this.color_b.Size = new System.Drawing.Size(90, 30);
             this.color_b.TabIndex = 12;
@@ -173,7 +199,7 @@
             // to_l
             // 
             this.to_l.AutoSize = true;
-            this.to_l.Location = new System.Drawing.Point(371, 59);
+            this.to_l.Location = new System.Drawing.Point(440, 58);
             this.to_l.Name = "to_l";
             this.to_l.Size = new System.Drawing.Size(19, 15);
             this.to_l.TabIndex = 11;
@@ -182,7 +208,7 @@
             // from_l
             // 
             this.from_l.AutoSize = true;
-            this.from_l.Location = new System.Drawing.Point(355, 29);
+            this.from_l.Location = new System.Drawing.Point(424, 28);
             this.from_l.Name = "from_l";
             this.from_l.Size = new System.Drawing.Size(35, 15);
             this.from_l.TabIndex = 10;
@@ -190,17 +216,19 @@
             // 
             // numericUpDown5
             // 
-            this.numericUpDown5.Location = new System.Drawing.Point(396, 57);
+            this.numericUpDown5.Location = new System.Drawing.Point(465, 56);
             this.numericUpDown5.Name = "numericUpDown5";
             this.numericUpDown5.Size = new System.Drawing.Size(66, 23);
             this.numericUpDown5.TabIndex = 6;
+            this.numericUpDown5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // numericUpDown4
             // 
-            this.numericUpDown4.Location = new System.Drawing.Point(396, 27);
+            this.numericUpDown4.Location = new System.Drawing.Point(465, 26);
             this.numericUpDown4.Name = "numericUpDown4";
             this.numericUpDown4.Size = new System.Drawing.Size(66, 23);
             this.numericUpDown4.TabIndex = 5;
+            this.numericUpDown4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // save_b
             // 
@@ -211,38 +239,49 @@
             this.save_b.Text = "Save";
             this.save_b.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // phases_lb
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(0, 3);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(299, 379);
-            this.listBox1.TabIndex = 0;
+            this.phases_lb.FormattingEnabled = true;
+            this.phases_lb.ItemHeight = 15;
+            this.phases_lb.Location = new System.Drawing.Point(0, 3);
+            this.phases_lb.Name = "phases_lb";
+            this.phases_lb.Size = new System.Drawing.Size(395, 184);
+            this.phases_lb.TabIndex = 0;
             // 
             // devices_p
             // 
-            this.devices_p.Controls.Add(this.listView2);
+            this.devices_p.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.devices_p.Controls.Add(this.update_animations_b);
+            this.devices_p.Controls.Add(this.animations_lv);
             this.devices_p.Controls.Add(this.textBox2);
             this.devices_p.Location = new System.Drawing.Point(572, 233);
             this.devices_p.Name = "devices_p";
             this.devices_p.Size = new System.Drawing.Size(200, 215);
             this.devices_p.TabIndex = 3;
             // 
-            // listView2
+            // animations_lv
             // 
-            this.listView2.Location = new System.Drawing.Point(3, 29);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(194, 187);
-            this.listView2.TabIndex = 1;
-            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.animations_lv.Location = new System.Drawing.Point(0, 29);
+            this.animations_lv.Name = "animations_lv";
+            this.animations_lv.Size = new System.Drawing.Size(197, 187);
+            this.animations_lv.TabIndex = 1;
+            this.animations_lv.UseCompatibleStateImageBehavior = false;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(0, 0);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(197, 23);
+            this.textBox2.Size = new System.Drawing.Size(136, 23);
             this.textBox2.TabIndex = 0;
+            // 
+            // update_animations_b
+            // 
+            this.update_animations_b.Location = new System.Drawing.Point(142, 0);
+            this.update_animations_b.Name = "update_animations_b";
+            this.update_animations_b.Size = new System.Drawing.Size(58, 23);
+            this.update_animations_b.TabIndex = 14;
+            this.update_animations_b.Text = "Update";
+            this.update_animations_b.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -253,6 +292,8 @@
             this.Controls.Add(this.animation_maker_p);
             this.Controls.Add(this.animation_settings_p);
             this.Controls.Add(this.animations_p);
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
             this.Name = "Form1";
             this.Text = "Form1";
             this.animations_p.ResumeLayout(false);
@@ -276,7 +317,7 @@
 
         private Panel animations_p;
         private TextBox textBox1;
-        private ListView listView1;
+        private ListView slaves_lv;
         private Panel animation_settings_p;
         private Label repetitions_l;
         private Label phases_l;
@@ -289,12 +330,15 @@
         private NumericUpDown numericUpDown5;
         private NumericUpDown numericUpDown4;
         private Button save_b;
-        private ListBox listBox1;
+        private ListBox phases_lb;
         private Panel devices_p;
-        private ListView listView2;
+        private ListView animations_lv;
         private TextBox textBox2;
         private TrackBar repetitions_tb;
         private Button color_b;
         private ColorDialog colorDialog1;
+        private ListBox colors_lb;
+        private Button update_b;
+        private Button update_animations_b;
     }
 }
--- Led_Animation_Editor/Form1.cs
+++ Led_Animation_Editor/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
 namespace Led_Animation_Editor
 {
     public partial class Form1 : Form
@@ -5,6 +10,78 @@ namespace Led_Animation_Editor
         public Form1()
         {
             InitializeComponent();
+            get_slaves();
         }
+
+        private void get_slaves()
+        {
+            // Create a buffer
+            byte[] buffer = new byte[1024];
+
+            // Create the socket
+            Socket socket = new Socket( SocketType.Stream, ProtocolType.Tcp );
+
+            // Connect to the server
+            try
+            {
+                // Connect to the server
+                IPEndPoint end_point = new IPEndPoint( IPAddress.Parse( "192.136.60.133" ), ( int )( 1235 ) );
+                socket.Connect( end_point );
+
+                // Ask for the slaves
+                socket.Send( Encoding.ASCII.GetBytes( "get_slaves" ) );
+
+                // Recive slaves
+                while ( socket.Receive( buffer ) > 0 )
+                {
+                    for ( Int32 i = 0; i < buffer.Length && buffer[i] != 0; i++ )
+                        slaves_lv.Items.Add( buffer[i].ToString() );
+                }
+
+                // Close the connection
+                socket.Shutdown( SocketShutdown.Both );
+                socket.Close();
+            }
+            catch ( Exception e )
+            {
+                MessageBox.Show( "Error while connecting" );
+            }
+        }
+
+        private void get_animations_names()
+        {
+            // Create a buffer
+            byte[] buffer = new byte[1024];
+
+            // Create the socket
+            Socket socket = new Socket( SocketType.Stream, ProtocolType.Tcp );
+
+            // Connect to the server
+            try
+            {
+                // Connect to the server
+                IPEndPoint end_point = new IPEndPoint( IPAddress.Parse( "192.136.60.133" ), ( int )( 1235 ) );
+                socket.Connect( end_point );
+
+                // Ask for the slaves
+                socket.Send( Encoding.ASCII.GetBytes( "get_animations_names" ) );
+
+                // Recive slaves
+                while ( socket.Receive( buffer ) > 0 )
+                {
+                }
+
+                // Close the connection
+                socket.Shutdown( SocketShutdown.Both );
+                socket.Close();
+            }
+            catch ( Exception e )
+            {
+                MessageBox.Show( "Error while connecting" );
+            }
+        }
+
+        private void get_animation()
+        {}
     }
 }
